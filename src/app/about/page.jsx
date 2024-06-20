3bcc6e30c05362298d6fac3a88e855e1ff877779
Chamika1016 <rclurathnaweera@std.appsc.sab.ac.lk>
2024-06-20 21:56:09 +0530

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4 ">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to IMDb Clone, your premier destination for all things movies
+        and TV shows! At IMDb Clone, we are dedicated to bringing you the latest
+        and greatest in cinema and television. Our team of passionate film and
+        TV enthusiasts works tirelessly to curate a vast collection of titles,
+        ranging from timeless classics to the newest releases, ensuring there's
+        something for every kind of viewer. Whether you're a casual watcher or a
+        serious cinephile, our goal is to elevate your viewing experience with
+        comprehensive reviews, insightful analyses, and the latest industry
+        news.
+      </p>
+      <p>
+        IMDb Clone was founded by a group of movie and TV buffs who share a deep
+        love for storytelling and the entertainment industry. Our mission is to
+        build a vibrant community where fans can discover new favorites, discuss
+        their thoughts, and celebrate the art of filmmaking and television
+        production. We believe that films and TV shows have the power to
+        inspire, entertain, and provoke thought, and we are committed to
+        highlighting works that embody these qualities. Our user-friendly
+        platform allows you to explore our extensive catalog, read expert
+        opinions, and join lively discussions with fellow fans.
+      </p>
+      <p>
+        At IMDb Clone, we are more than just a website; we are a community
+        driven by a shared passion for movies and TV shows. Our team is
+        constantly working to bring you top-notch content, from exclusive
+        interviews with industry insiders to behind-the-scenes looks at upcoming
+        releases and carefully curated recommendations tailored to your tastes.
+        Join us on this exciting journey, and let's celebrate the magic of film
+        and television together!
+      </p>
+    </div>
+  );
+};
+
+export default page;
